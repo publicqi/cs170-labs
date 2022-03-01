746395eb7d8e5f0bad5734a46c70ea36e39145d1
publicqi <qisu@ucsb.edu>
2022-02-28 18:12:15 -0800

    fix bug

--- lab4/schedos.h
+++ lab4/schedos.h
@@ -17,8 +17,8 @@
 #define INT_SYS_USER1		50
 #define INT_SYS_USER2		51
 
-#define INT_SYS_PRIORITY    0x1337
-#define INT_SYS_PUTCHAR     0x1338
+#define INT_SYS_PRIORITY    52
+#define INT_SYS_PUTCHAR     53
 
 
 // The current screen cursor position (stored at memory location 0x198000).
